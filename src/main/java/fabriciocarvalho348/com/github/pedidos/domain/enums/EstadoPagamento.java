69c11b01a93d802f8bf1cd5c96ae7d3e54792148
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2024-01-12 08:34:23 -0300

    feat: Adicionando entidades de pagamento, enum de pagamento para definir o estado do pagamento e adicionando exceptions para objetos não encontrados e de integridade de dados

---
+++ src/main/java/fabriciocarvalho348/com/github/pedidos/domain/enums/EstadoPagamento.java
@@ -0,0 +1,37 @@
+package fabriciocarvalho348.com.github.pedidos.domain.enums;
+
+public enum EstadoPagamento {
+    PENDENTE(1, "Pendente"),
+    QUITADO(2, "Quitado"),
+    CANCELADO(3, "Cancelado");
+
+    private int cod;
+    private String descricao;
+
+    EstadoPagamento(int cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public static EstadoPagamento toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+
+        for (EstadoPagamento x : EstadoPagamento.values()) {
+            if (cod.equals(x.getCod())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Id inválido: " + cod);
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
